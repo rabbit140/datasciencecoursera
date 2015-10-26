df3e70f9adda8649f26f229b0d549230af577bd1
rabbit140 <p.zientala.1995@gmail.com>
2015-10-26 18:11:10 +0000

    Code tweak

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,11 @@ if(sum(package_names == c("data.table")) == 0) {
 
 suppressMessages(library(data.table))
 
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+file <- "downloaded_data.zip"
+download.file(url, file)
+unzip(file, exdir = getwd())
+
 #Gets working directory and set appropriate file paths
 filePath <- getwd()
 filePath <- paste(filePath, "/UCI HAR Dataset", sep = "")
